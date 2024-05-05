663c7c7dd811abc62221b170b2102dc4c621fb06
SK0528JP <142498919+SK0528JP@users.noreply.github.com>
2024-05-06 00:32:07 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-/* script.js */
 document.getElementById('logo').addEventListener('click', function() {
+  console.log("Logo clicked"); // クリックされたことをコンソールに表示
   var audio = document.getElementById('audio');
   audio.play(); // 音声を再生
 });
