34e1115c7008b73fc883f1253f0c252daf217271
SK0528JP <142498919+SK0528JP@users.noreply.github.com>
2024-09-06 21:57:04 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const logoImage = document.getElementById('logoImage');
-const audioPlayer = document.getElementById('audioPlayer');
-
-logoImage.addEventListener('click', () => {
-  audioPlayer.play();
-});
